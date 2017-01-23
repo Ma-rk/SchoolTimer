17716222404f4896fc587bee31573b9cbacc7bca
kmark <kmark@DESKTOP-5BOVIJQ>
2017-01-24 00:55:37 +0900

    add test code.

---
+++ SchoolTimerTests/SchoolTimerTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace SchoolTimer.Tests
+{
+    [TestClass()]
+    public class SchoolTimerTests
+    {
+        [TestMethod()]
+        public void CheckWorkingHourTest()
+        {
+            SchoolTimer timer = new SchoolTimer();
+
+            DateTime dt = new DateTime(2017, 1, 1, 0, 0, 0);
+            Assert.IsFalse(timer.CheckWorkingHour(dt));
+
+            dt = new DateTime(2017, 1, 1, 7, 59, 59);
+            Assert.IsFalse(timer.CheckWorkingHour(dt));
+
+            dt = new DateTime(2017, 1, 1, 8, 0, 0);
+            Assert.IsTrue(timer.CheckWorkingHour(dt));
+
+            dt = new DateTime(2017, 1, 1, 8, 0, 1);
+            Assert.IsTrue(timer.CheckWorkingHour(dt));
+
+            dt = new DateTime(2017, 1, 1, 12, 0, 0);
+            Assert.IsTrue(timer.CheckWorkingHour(dt));
+
+            dt = new DateTime(2017, 1, 1, 17, 59, 59);
+            Assert.IsTrue(timer.CheckWorkingHour(dt));
+
+            dt = new DateTime(2017, 1, 1, 18, 0, 0);
+            Assert.IsFalse(timer.CheckWorkingHour(dt));
+
+            dt = new DateTime(2017, 1, 1, 18, 0, 1);
+            Assert.IsFalse(timer.CheckWorkingHour(dt));
+
+            dt = new DateTime(2017, 1, 1, 23, 59, 59);
+            Assert.IsFalse(timer.CheckWorkingHour(dt));
+        }
+    }
+}
