799357fb7920374aa741b660c5cebf03643d4cd7
kmark <kmark@DESKTOP-5BOVIJQ>
2017-02-14 23:42:21 +0900

    display time.

--- SchoolTimerWpf/MainWindow.xaml.cs
+++ SchoolTimerWpf/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace SchoolTimerWpf
 {
@@ -23,6 +12,38 @@ namespace SchoolTimerWpf
         public MainWindow()
         {
             InitializeComponent();
+
+            DispatcherTimer timer = new DispatcherTimer();
+
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Start();
+        }
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            label_currentTime.Content = DateTime.Now.ToLongTimeString();
+
+            int min = DateTime.Now.Minute;
+            if (min < 45)
+                Do_sprint();
+            else
+                Do_break();
+        }
+        private void Do_sprint()
+        {
+            label_break.Content = "15:00";
+
+            label_sprint.Content = string.Format("{0}:{1}"
+                                            , DateTime.Now.Minute.ToString("D2")
+                                            , DateTime.Now.Second.ToString("D2"));
+        }
+        private void Do_break()
+        {
+            label_sprint.Content = "45:00";
+
+            label_break.Content = string.Format("{0}:{1}"
+                                            , (DateTime.Now.Minute - 45).ToString("D2")
+                                            , DateTime.Now.Second.ToString("D2"));
         }
     }
 }
